0d2b881547c2a2c11307ceb4b94ff51885c11ef8
Dinesh Channa <dchanna@factset.com>
2020-08-01 21:39:45 +0530

    modified

--- S3/provider.tf
+++ S3/provider.tf
@@ -1,5 +1,5 @@
 provider "aws"{
     version = "~> 2.0"
     region= var.reg
-	shared_credentials_file="C:\Users\Administrator\.aws\credentials"
+	shared_credentials_file="C:/Users/Administrator/.aws/credentials"
 }
